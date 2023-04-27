979214e65044ae585c4232de0997b5eef2d1badd
Dae Hyeon <125842647+Ukidely@users.noreply.github.com>
2023-04-27 22:27:23 +0900

    [ADD 제너릭
    - 제너릭 사용법

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,52 +1,13 @@
 fun main() {
 	
-	val dog = Dog()
-	val cat = Cat()
+	// 타입추론도 지원
+	val box: Box<Int> = Box(10)
+	val box2 = Box("dfdf")
 	
-	// 	타입 체크는 is를 사용함
-	//  as로 타입 캐스팅도 가능
-	if (dog is Animal) {
-		println("dog is Animal")
-	}
+	println(box.value)
+	println(box2.value)
 }
 
 
-// 추상 클래스
-abstract class Animal {
-	// open 키워드를 사용해서 override를 허용한다.
-	open fun move() {
-		println("이동")
-	}
-}
-
-
-// 상속을 할때는 클래스이름() <- 소괄호를 넣어야한다.
-class Dog : Animal() {
-	override fun move() {
-		println("터벅")
-	}
-}
-
-// 인터페이스
-interface Drawable {
-	fun draw()
-}
-
-
-// 인터페이스를 상속받을때는 , 를 사용해서 여러개를 상속받을 수 있다.
-class Cat : Animal(), Drawable {
-	override fun move() {
-		println("살금")
-	}
-	
-	override fun draw() {
-		println("뭔갈 그리다")
-	}
-}
-
-
-// 일반 class는 상속이 불가능하다
-// 하지만 open 키워드를 사용하면 상속이 가능하다.
-open class Person
-
-class SuperMan : Person()
+// 제너릭 사용법
+class Box<T>(val value: T)
