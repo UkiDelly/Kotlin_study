8cb20320ad3b50b8efd119132960b68ac73558fe
Dae Hyeon <125842647+Ukidely@users.noreply.github.com>
2023-04-27 20:55:38 +0900

    [ADD] for문
    - for in 반복문
    - forEach 반복문
    - 일반적인 for문

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,68 +1,21 @@
 fun main() {
-	// 조건문
-	var i = 5
-	if (i > 10) {
-		println("10 보다 크다")
-	} else if (i > 5) {
-		println("5보다 크다")
-	} else {
-		println("5보다 작거나 같다")
+	
+	// 반복문
+	
+	// List 생성 (Array 아님)
+	val items = listOf(1, 2, 3, 4, 5)
+	
+	// for in 반복문
+	for (item in items) {
+		println(item)
 	}
 	
-	// if는 when으로 치환 가능
-	
-	when {
-		i > 10 -> {
-			println("10 보다 크다")
-		}
-		
-		i > 5 -> {
-			println("5보다 크다")
-		}
-		
-		else -> {
-			println("5보다 작거나 같다")
-		}
+	// 	forEach 반복문
+	items.forEach { item ->
+		println(item)
 	}
 	
-	// if문을 변수에 넣을수도 있다
-	var result = if (i is Int) {
-		
-		// i가 정수 일경우 "Int"문자열을 반환
-		"Int"
-	} else {
-		
-		// i가 정수가 아닐경우 "not Int"문자열을 반환
-		"not Int"
-	}
-	
-	println(result)
-	
-	// when문
-	var result2 = when (i) {
-		1 -> {
-			"i == 1"
-		}
-		
-		2 -> {
-			"i == 2"
-		}
-		
-		else -> {
-			"i is neither 1 nor 2"
-		}
-	}
-	
-	println(result2)
-	
-	
-	// 	삼항 연산
-	i = 10
-	val result3: Boolean = i > 10
-	// 	또는
-	val result4: Boolean = i > 10
-	
-	println(result3)
-	println(result4)
-	
+	// 	일반적인 for문
+	for (i in 0..items.size)
+		println(i)
 }
