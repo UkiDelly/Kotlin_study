4d6903d1ef4c2c85c714ac5bdb4063708d9f6d99
Dae Hyeon <125842647+Ukidely@users.noreply.github.com>
2023-04-27 21:13:11 +0900

    [ADD 배열
    - 특정 위치에 값 넣기
    - 특정 위치의 값 가져오기

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,18 +1,10 @@
 fun main() {
-	// 	List 타입
+	// 배열 (크기가 지정된 List이라고 생각하면 될듯), 배열은 잘 사용 안함
+	val items: Array<Int> = arrayOf(1, 2, 3)
 	
-	// 변경이 안되는 List (읽기만 가능)
-	val items = listOf(1, 2, 3, 4, 5)
+	// 특정 위치에 값 넣기 (List도 같음)
+	items[0] = 10
 	
-	//! 에러. 읽기만 가능
-	items.add(3)
-	
-	// 	변경이 가능한 List (읽기/쓰기 가능)
-	val newItems: MutableList<Int> = mutableListOf(1, 2, 3, 4, 5)
-	
-	// 에러 발생 X 읽기,쓰기 가능
-	newItems.add(6)
-	newItems.remove(3)
-	
-	
+	// 특정 위치의 값 가져오기 (List도 같음)
+	println(items[0])
 }
