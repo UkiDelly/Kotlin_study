11c798dc980f857d8f540e8ffe04903b5f1f6532
Dae Hyeon <125842647+Ukidely@users.noreply.github.com>
2023-04-27 21:49:29 +0900

    [ADD] 함수
    - 함수

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,24 +1,18 @@
 fun main() {
-	// null safety
-	var name: String? = null
+	println(sum(10, 20))
+	println(sum2(10, 20))
 	
-	name = "대현"
-	
-	// 수식객체 함수
-	"종인".also { name = it }
-	name = null
-	
-	
-	var name2: String = ""
-	
-	// null이 아닐때
-	name2 = name !!
-	
-	
-	// 	코틀린 내장 널체크
-	// name이 널이 아니라면,
-	name?.let {
-		name2 = name !!
-	}
-	
+	// 	named 변수처럼 쓸수도 있다
+	println(sum2(b = 10, a = 20))
 }
+
+
+// 일반적인 함수 (전역)
+fun sum(a: Int, b: Int): Int {
+	return a + b
+}
+
+// 간략버전
+// 타입 추론이 가능하기 때문에 리턴값 명시를 생략해도 된다
+fun sum2(a: Int, b: Int) = a + b
+
