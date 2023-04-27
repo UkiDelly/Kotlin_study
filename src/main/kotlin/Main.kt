21d86d3c357afdc503590682302bc4384d883f27
Dae Hyeon <125842647+Ukidely@users.noreply.github.com>
2023-04-27 20:55:38 +0900

    [UPDATE] for 문
    - for문 label 기능 추가

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,37 +1,44 @@
 fun main() {
 	
-	// 콜백 함수를 넣을때는 {}를 사용
-	myFunc({ println("함수 호출") })
+	// 반복문
 	
-	// 	인자가 함수인경우, 밖으로 빼서 사용 가능
-	//  인자가 함수 하나인 경우 ()도 생략 가능
-	myFunc { println("함수 호출") }
+	// List 생성 (Array 아님)
+	val items = listOf(1, 2, 3, 4, 5)
 	
-	// 인자가 여러개인 경우, ()를 생략할 수 없다.
-	myFunc2(10) { println("함수 호출") }
+	// for in 반복문
+	for (item in items) {
+		println(item)
+	}
 	
+	// 	forEach 반복문
+	items.forEach { item ->
+		println(item)
+	}
 	
-	// 정지 함수는 코루틴에서만 사용 가능
-	myFunc3()
-}
-
-
-// 콜백 함수 받는 법
-// 코틀린에는 void가 없고 대신 Unit이라는 특수한 타입이 있다.
-fun myFunc(callBack: () -> Unit) {
-	println("myFunc 호출")
-	callBack()
-	println("myFunc 끝")
-}
-
-fun myFunc2(a: Int, callBack: () -> Unit) {
-	println("myFunc2 호출")
-	println(a)
-	callBack()
-	println("myFunc2 끝")
-}
-
-// 정지 함수. suspend 키워드를 붙여준다.
-suspend fun myFunc3() {
-	println("myFunc3 호출")
+	// 	일반적인 for문
+	for (i in 0..items.size)
+		println(i)
+	
+	// 단계 지정
+	for (i in 0..10 step 2)
+		println(i)
+	
+	// 역방향
+	for (i in 10 downTo 0)
+		println(i)
+	
+	// 범위 지정 0..10 와 같은것으로 생각됨
+	for (i in 0 until 10)
+		println(i)
+	
+	// 다중 for문에서 for문 특정
+	loop@ for (i in 1..100) {
+		for (j in 1..100) {
+			if (i == 10 && j == 10) {
+				
+				// @loop 라벨을 지정한 for문을 종료
+				break@loop
+			}
+		}
+	}
 }
