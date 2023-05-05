ff7b84c933e84128f9ed731c6aa84245dbaa27f3
Dae Hyeon <daehyeonsong@fashionandstyle.com>
2023-05-05 22:46:40 +0900

    [ADD] Generic
    - 제너릭 사용법

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,40 +1,40 @@
 fun main() {
-	//  as , is
-	val a = Drink()
-	a.drink()
 	
-	// b는 Cola 타입이지만 Drink 타입으로 선언되었기 때문에
-	// Cola의 washDishes() 메서드를 사용할 수 없다.
-	val b: Drink = Cola()
-	b.drink()
+	UsingGeneric(A()).doShouting()
 	
-	
-	if (b is Cola) {
-		b.washDishes()
-	}
-	
-	val c = b as Cola
-	c.washDishes()
-	b.washDishes()
+	doShouting(B())
 	
 }
 
-open class Drink {
-	val name = "음료"
-	
-	open fun drink() {
-		println("${name}를 마십니다.")
+open class A {
+	open fun shout() {
+		println("A 가 소리칩니다")
 	}
 }
 
-class Cola : Drink() {
-	val type = "콜라"
-	
-	override fun drink() {
-		println("${name}중에 ${type}를 마십니다.")
+class B : A() {
+	override fun shout() {
+		println("B 가 소리칩니다")
 	}
-	
-	fun washDishes() {
-		println("${type}로 설거지를 합니다.")
+}
+
+class C : A() {
+	override fun shout() {
+		println("C 가 소리칩니다")
 	}
+}
+
+
+/**
+ *
+ * [T:A] 는 T는 A와 A의 자식 클래스만 가능하다는 뜻
+ */
+class UsingGeneric<T : A>(val t: T) {
+	fun doShouting() {
+		t.shout()
+	}
+}
+
+fun <T : A> doShouting(t: T) {
+	t.shout()
 }
