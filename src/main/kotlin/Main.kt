8dbeb1b5d908069af9c5d98dd1c1faf60beaef6d
Dae Hyeon <daehyeonsong@fashionandstyle.com>
2023-05-05 23:13:45 +0900

    [ADD] 문자열 다루기
    - length
    - lowercase
    - uppercase
    - split
    - joinToString
    - substring
    - isNullOrEmpty
    - isNullOrBlank
    - startsWith
    - endsWith
    - contains

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,31 +1,58 @@
 fun main() {
+	// 문자열 다루기
+	val test1 = "Test.Kotlin.String"
 	
-	// 	 List
-	val a = listOf("사과", "배", "딸기", "포도")
+	println(test1.length)
+	println(test1.lowercase())
+	println(test1.uppercase())
 	
-	println(a[0])
+	// split
+	var test2 = test1.split(".")
 	
+	// join
+	println(test2.joinToString())
+	println(test2.joinToString("-"))
 	
-	val b = mutableListOf(6, 3, 1)
-	
-	println(b)
-	
-	b.add(4)
-	
-	println(b)
-	
-	b.add(2, 8)
-	
-	println(b)
-	
-	b.removeAt(1)
+	// 	 substring
+	println(test1.substring(5..10))
 	
 	
-	println(b)
+	//
+	val nullString: String? = null
+	val emptyString = ""
+	val blankString = " "
+	val normalString = "a"
 	
-	b.shuffle()
-	println(b)
+	//  null of empty
+	println(nullString.isNullOrEmpty())
+	println(emptyString.isNullOrEmpty())
+	println(blankString.isNullOrEmpty())
+	println(normalString.isNullOrEmpty())
 	
-	b.sort()
-	println(b)
+	println()
+	
+	// null or blank
+	println(nullString.isNullOrBlank())
+	println(emptyString.isNullOrBlank())
+	println(blankString.isNullOrBlank())
+	println(normalString.isNullOrBlank())
+	
+	println()
+	
+	// 특정 문자열로 시작하거나 끝나는지 확인
+	val test3 = "kotlin.kt"
+	val test4 = "java.java"
+	
+	println(test3.startsWith("java"))
+	println(test4.startsWith("java"))
+	
+	println(test3.endsWith(".kt"))
+	println(test4.endsWith(".kt"))
+	
+	println(test3.contains("lin"))
+	println(test4.contains("lin"))
+	
+	println()
+	
+	
 }
