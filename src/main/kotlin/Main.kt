9899b249f62b620da562e7db0f75549b291e0014
Dae Hyeon <daehyeon1999@naver.com>
2023-05-13 18:54:35 +0900

    [ADD] collection 함수 2
    - associateBy
    - groupBy
    - partition
    - flatMap
    - getOrElst
    - zip

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,41 +1,46 @@
 fun main() {
 	
 	/**
-	 *  [forEach] - collection내의 모든 요소에 대해 동일한 작업을 수행할 때 사용
-	 *  [filter] - collection내의 요소 중 특정 조건을 만족하는 요소만을 추출하여 반환할때 사용
-	 *  [map] - collection내의 각각 요소에 대해 동일한 작업을 수행하고, 그 결과를 새로운 collection으로 반환할 때 사용
-	 *  [any] - collection내의 요소 중 특정 조건을 만족하는 요소가 하나라도 존재하는지 여부를 반환할 때 사용
-	 *  [all] - collection내의 모든 요소가 특정 조건을 만족하는지 여부를 반환할 때 사용
-	 *  [none] - collection내의 요소 중 특정 조건을 만족하는 요소가 하나도 존재하지 않는지 여부를 반환할 때 사용
-	 *  [first] - collection내의 첫번째 요소를 반환할 때 사용, 람다 함수 안에 특정 조건을 추가하면 조건을 만족하는 첫번째 요소를 반환
-	 *  [firstOrNull] - first를 대체하여 사용 가능, 조건을 만족하는 요소가 없을 경우 null을 반환
-	 *  [last] - collection내의 마지막 요소를 반환할 때 사용, 람다 함수 안에 특정 조건을 추가하면 조건을 만족하는 마지막 요소를 반환
-	 *  [lastOrNull] - last를 대체하여 사용 가능, 조건을 만족하는 요소가 없을 경우 null을 반환
-	 *  [find] - first를 대체하여 사용 가능
-	 *  [findLast] - last를 대체하여 사용 가능
-	 *  [count] - collection내의 요소의 개수 반환, 람다 함수내에 특정 조건을 걸 경우, 조건을 만족하는 요소의 개수를 반환할 때 사용
+	 *  [associateBy] - List내의 요소들을, 해당 요소의 속성들을 key로 만들어 Map으로 반환
+	 *  [groupBy] - key가 같은 요소들을 배열로 묶어 Map으로 반환
+	 *  [partition] - 조건에 따라 List를 2개로 나누어 Pair로 반환
+	 *  [flatMap] - 요소마다 만들어진 컬렉션을 합쳐서 반환
+	 *  [getOrElse] - index 위치에 아이템이 있으면 아이템을 반환하고, 아닌 경우 지정한 기본값을 반환
+	 *  [zip] - 컬렉션 두개의 아이템을 1:1로 매칭하여 개 컬렉션을 만들어줌
 	 */
 	
-	val nameList = listOf("박수영", "김지수", "김다현", "신유나", "김지우")
+	val personList = listOf(
+		Person("유나", 1992),
+		Person("조이", 1996),
+		Person("츄", 1999),
+		Person("유나", 2003)
+	)
 	
-	nameList.forEach { print(it + ", ") }
-	println()
 	
-	println(nameList.filter { it.startsWith("김") })
+	println(personList.associateBy { it.birthYear })
 	
-	println(nameList.map { "이름 : $it" })
+	println(personList.groupBy { it.name })
 	
-	println(nameList.any { it == "김지연" })
+	val (over98, under98) = personList.partition { it.birthYear > 1998 }
+	println(over98)
+	println(under98)
 	
-	println(nameList.all { it.length == 3 })
 	
-	println(nameList.none { it.startsWith("이") })
+	// 	========================================================================
 	
-	println(nameList.first { it.startsWith("김") })
+	val numbers = listOf(- 3, 7, 2, - 10, 1)
 	
-	println(nameList.last { it.startsWith("김") })
+	println(numbers.flatMap { listOf(it * 10, it + 10) })
 	
-	println(nameList.count { it.contains("지") })
+	println(numbers.getOrElse(1) { 50 })
 	
+	println(numbers.getOrElse(10) { 50 })
+	
+	val names = listOf("A", "B", "C", "D")
+	
+	print(names zip numbers)
 	
 }
+
+
+data class Person(val name: String, val birthYear: Int)
