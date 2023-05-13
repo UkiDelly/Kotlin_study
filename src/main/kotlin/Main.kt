f33c201853c70cd55dbc19e85db7b0fa3ac459fb
Dae Hyeon <daehyeon1999@naver.com>
2023-05-13 18:39:55 +0900

    [ADD] collection 함수
    - forEach
    - map
    - any
    - all
    - none
    - first
    - firstOrNull
    - last
    - lastOrNull
    - find
    - findLast
    - count

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,59 +1,41 @@
 fun main() {
 	
 	/**
-	 *  [Set] - 정렬되어 있지 않으며 중복을 허락하지 않는 Collection
-	 *        - index으로 값을 참조할 수 없다.
-	 *        - set.contains(element) : element가 set에 포함되어 있는지 확인
-	 *        - setOf() : 불변형 Set을 생성
-	 *        - mutableSetOf() : 가변형 Set을 생성
-	 */
-	val set = mutableSetOf<String>("귤", "바나나", "키워")
-	
-	for (item in set) {
-		println(item)
-	}
-	
-	set.add("자몽")
-	println(set)
-	
-	set.remove("바나나")
-	println(set)
-	
-	println(set.contains("귤"))
-	
-	
-	/**
-	 *  [Map] - Key와 Value의 쌍으로 이루어진 Collection
-	 *        - map[key] = value : key에 value를 할당
-	 *        - mapOf() : 불변형 Map을 생성
-	 *        - mutableMapOf() : 가변형 Map을 생성
-	 *        - put(key, value) : key에 value를 할당
-	 *        - remove(key) : key에 해당하는 값을 제거
-	 *        - Dart와는 달리 : 가 아닌 to를 사용한다.
+	 *  [forEach] - collection내의 모든 요소에 대해 동일한 작업을 수행할 때 사용
+	 *  [filter] - collection내의 요소 중 특정 조건을 만족하는 요소만을 추출하여 반환할때 사용
+	 *  [map] - collection내의 각각 요소에 대해 동일한 작업을 수행하고, 그 결과를 새로운 collection으로 반환할 때 사용
+	 *  [any] - collection내의 요소 중 특정 조건을 만족하는 요소가 하나라도 존재하는지 여부를 반환할 때 사용
+	 *  [all] - collection내의 모든 요소가 특정 조건을 만족하는지 여부를 반환할 때 사용
+	 *  [none] - collection내의 요소 중 특정 조건을 만족하는 요소가 하나도 존재하지 않는지 여부를 반환할 때 사용
+	 *  [first] - collection내의 첫번째 요소를 반환할 때 사용, 람다 함수 안에 특정 조건을 추가하면 조건을 만족하는 첫번째 요소를 반환
+	 *  [firstOrNull] - first를 대체하여 사용 가능, 조건을 만족하는 요소가 없을 경우 null을 반환
+	 *  [last] - collection내의 마지막 요소를 반환할 때 사용, 람다 함수 안에 특정 조건을 추가하면 조건을 만족하는 마지막 요소를 반환
+	 *  [lastOrNull] - last를 대체하여 사용 가능, 조건을 만족하는 요소가 없을 경우 null을 반환
+	 *  [find] - first를 대체하여 사용 가능
+	 *  [findLast] - last를 대체하여 사용 가능
+	 *  [count] - collection내의 요소의 개수 반환, 람다 함수내에 특정 조건을 걸 경우, 조건을 만족하는 요소의 개수를 반환할 때 사용
 	 */
 	
-	val map = mutableMapOf<String, String>(
-		"레드벨벳" to "음파음파",
-		"트와이스" to "FANCY",
-		"ITZY" to "ICY"
-	)
+	val nameList = listOf("박수영", "김지수", "김다현", "신유나", "김지우")
 	
+	nameList.forEach { print(it + ", ") }
+	println()
 	
-	for (entry in map) {
-		println("${entry.key} : ${entry.value}")
-	}
+	println(nameList.filter { it.startsWith("김") })
 	
+	println(nameList.map { "이름 : $it" })
 	
-	// 추가
-	map.put("오마이걸", "번지")
-	println(map)
+	println(nameList.any { it == "김지연" })
 	
-	// 제거
-	map.remove("ITZY")
-	println(map)
+	println(nameList.all { it.length == 3 })
 	
+	println(nameList.none { it.startsWith("이") })
 	
-	// 값 참조
-	println(map["레드벨벳"])
+	println(nameList.first { it.startsWith("김") })
+	
+	println(nameList.last { it.startsWith("김") })
+	
+	println(nameList.count { it.contains("지") })
+	
 	
 }
