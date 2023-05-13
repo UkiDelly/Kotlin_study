c39b20b5ee5610bea903aa930ac56dfdcdac1ec8
Dae Hyeon <daehyeon1999@naver.com>
2023-05-13 18:12:40 +0900

    [ADD] data class, enum class
    - data class의 사용법 및 일반 클래스와의 차이점
    - enum의 사용법

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,60 +1,74 @@
 fun main() {
 	
-	Outer.Nested().introduce()
+	// 일반적인 class와 data class의 차이점 확인
+	val a = General("보영", 212)
 	
-	val nested = Outer.Nested()
-	nested.introduce()
+	println(a == General("보영", 212)) // false
+	println(a.hashCode())
+	println(a)
 	
-	val outer = Outer()
-	val innerClass = outer.Inner()
-	innerClass.introduceInner()
-	innerClass.introduceOuter()
 	
-	outer.text = "Changed Outer Class"
-	innerClass.introduceOuter()
+	val b = Data("루다", 306)
+	
+	println(b == Data("루다", 306)) // true
+	println(b.hashCode())
+	println(b)
+	
+	
+	println(b.copy())
+	println(b.copy("아린"))
+	println(b.copy(id = 618))
+	
+	
+	// 	componentX() 함수의 사용
+	val list = listOf(
+		Data("보영", 212),
+		Data("루다", 306),
+		Data("아린", 618)
+	)
+	
+	
+	// 이렇게 for문을 설정하면 내부적으로 componentX() 함수를 사용한다.
+	for ((a, b) in list) {
+		println("${a}, ${b}")
+	}
+	
+	// 	enum 활용
+	var state = State.SING
+	println(state)
+	
+	
+	state = State.SLEEP
+	println(state.isSleeping())
+	
+	state = State.EAT
+	println(state.message)
 	
 }
 
 
-// 중첩 클래스 / Nested Class , 내부 클래스 / Inner Class
-class Outer {
-	var text = "Outer Class"
-	
-	
-	/**
-	 *  [Nested] - 중첩 클래스로, Outer.Nested()으로 접근이 가능하다.
-	 *             특징
-	 *                1. Outer 클래스의 멤버에 접근이 가능하다.
-	 *                2. Nestes -> Outer 멤버 접근이 가능하지만 Outer -> Nested 멤버 접근이 불가능하다.
-	 */
-	class Nested {
-		fun introduce() {
-			println("Nested Class")
-		}
-	}
-	
-	
-	/**
-	 *  [Inner] - 내부 클래스로, Outer().Inner()으로 접근이 가능하다
-	 *            특징
-	 * 	            1. Outer 클래스의 멤버에 접근이 가능하다.
-	 * 	            2. Nestes -> Outer , Outer -> Nested 멤버 접근이 가능하다
-	 */
-	inner class Inner {
-		val text = "Inner Class"
-		
-		fun introduceInner() {
-			println(text)
-		}
-		
-		fun introduceOuter() {
-			
-			// Outer Class의 멤버에 접근이 가능하다.
-			// Outer Class의 멤버와 동일한 이름의 멤버를 가지고 있다면
-			// 접근하기 위해서는 this@{Outer 클래스 이름}.{변수 이름}으로 접근이 가능하다.
-			println(this@Outer.text)
-		}
-	}
-}
+// Data class
+// 데이터를 다룰때 쓰기 좋음
+// toString(), equals(), hashCode(), copy(), componentX() 등을 자동으로 생성해줌
+// componentX() 함수의 경우, 속성을 순서대로 반환해주는 함수이다.
+/**
+ *  예) Data("A",7) 이라는 객체가 있을 때
+ *  Data.component1() 은 "A" 를 반환
+ *  Data.component2() 는 7 을 반환
+ */
+class General(val name: String, val id: Int)
 
-// 내부 클래스
+data class Data(val name: String, val id: Int)
+
+// Enum
+// 열거형 클래스
+// Dart의 Enum이랑 비슷함
+
+enum class State(val message: String) {
+	SING("노래를 부릅니다."),
+	EAT("밥을 먹습니다."),
+	SLEEP("잠을 잡니다.");
+	
+	
+	fun isSleeping() = this == SLEEP
+}
