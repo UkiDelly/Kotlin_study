54a34f3a731cd9e0e08f91a4ff34ee640193855f
Dae Hyeon <daehyeonsong@fashionandstyle.com>
2023-05-13 17:14:09 +0900

    [ADD] 중첩/내부 클래스
    - 중첩 클래스
    - 내부 클래스

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,52 +1,60 @@
 fun main() {
-	read(7)
-	read("감사합니다")
 	
-	deliveryItem("짬뽕")
-	deliveryItem("책", 3)
-	deliveryItem("노트북", 30, "학교")
-	deliveryItem("선물", destination = "친구집")
+	Outer.Nested().introduce()
 	
+	val nested = Outer.Nested()
+	nested.introduce()
 	
-	// vararg를 사용할때 vararg를 제일 마지막에 써야함
-	sum(1, 2, 3, 4)
+	val outer = Outer()
+	val innerClass = outer.Inner()
+	innerClass.introduceInner()
+	innerClass.introduceOuter()
 	
+	outer.text = "Changed Outer Class"
+	innerClass.introduceOuter()
 	
-	// 	infix 함수 사용
-	// Dart와는 달리 .을 사용하지 않고 사용가능한것으로 보임
-	println(6 multiply 4)
-	// 	그냥 일반적일 infix 사용법
-	println(6.multiply(4))
-}
-
-// 함수 오버라이딩
-fun read(x: Int) {
-	println("Int: $x")
-}
-
-fun read(x: String) {
-	println("String: $x")
-}
-
-// default argunments
-fun deliveryItem(name: String, count: Int = 1, destination: String = "집") {
-	println("$name, $count 개를 ${destination}에 배달하였습니다.")
 }
 
 
-// vairable number of arguments
-// 갯수 상관없이 같은 자료형의 인자를 받고 싶을때 varargs 사용
-fun sum(vararg numbers: Int) {
-	var sum = 0
+// 중첩 클래스 / Nested Class , 내부 클래스 / Inner Class
+class Outer {
+	var text = "Outer Class"
 	
-	for (n in numbers) {
-		sum += n
+	
+	/**
+	 *  [Nested] - 중첩 클래스로, Outer.Nested()으로 접근이 가능하다.
+	 *             특징
+	 *                1. Outer 클래스의 멤버에 접근이 가능하다.
+	 *                2. Nestes -> Outer 멤버 접근이 가능하지만 Outer -> Nested 멤버 접근이 불가능하다.
+	 */
+	class Nested {
+		fun introduce() {
+			println("Nested Class")
+		}
 	}
 	
-	println(sum)
+	
+	/**
+	 *  [Inner] - 내부 클래스로, Outer().Inner()으로 접근이 가능하다
+	 *            특징
+	 * 	            1. Outer 클래스의 멤버에 접근이 가능하다.
+	 * 	            2. Nestes -> Outer , Outer -> Nested 멤버 접근이 가능하다
+	 */
+	inner class Inner {
+		val text = "Inner Class"
+		
+		fun introduceInner() {
+			println(text)
+		}
+		
+		fun introduceOuter() {
+			
+			// Outer Class의 멤버에 접근이 가능하다.
+			// Outer Class의 멤버와 동일한 이름의 멤버를 가지고 있다면
+			// 접근하기 위해서는 this@{Outer 클래스 이름}.{변수 이름}으로 접근이 가능하다.
+			println(this@Outer.text)
+		}
+	}
 }
 
-// infix
-// Int의 infix 함수 생성
-// Dart의 extension과 비슷한 기능인듯
-infix fun Int.multiply(x: Int): Int = this * x
+// 내부 클래스
