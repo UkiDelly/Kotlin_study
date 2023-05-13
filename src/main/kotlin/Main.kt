264d3b00869397795a49814e30f437481e3cb4e5
Dae Hyeon <daehyeon1999@naver.com>
2023-05-13 20:53:46 +0900

    [ADD] 초기화 지연
    - lateinit
    - lazy

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,34 +1,46 @@
 fun main() {
+	val a = LateInitSample()
 	
-	val foodCourt = FoodCourt()
+	println(a.getLateInitText())
 	
-	foodCourt.searchPrice(FoodCourt.FOOD_CREAM_PASTA)
-	foodCourt.searchPrice(FoodCourt.FOOD_STEAK)
-	foodCourt.searchPrice(FoodCourt.FOOD_PIZZA)
+	a.text = "새로 할당한 값"
 	
+	println(a.getLateInitText())
+	
+	
+	// 	============================
+	
+	
+	// 	lazy
+	// 	초기화를 늦게 진행하는 속성
+	// 	by lazy { 초기화 내용 }으로 구성되며 마지막으로 실행한 구문을 반환
+	// 	생성시 초기화가 아닌 해당 속성을 처음 사용했을때 초기화해줌
+	val number: Int by lazy {
+		println("초기화를 합니다")
+		7
+	}
+	
+	println("코드를 시작합니다")
+	// 이때 해당 변수/속성을 처음 사용하므로 이때 초기화를 진행
+	println(number)
+	// 이미 초기화 했기 때문에 초기화 구문을 실행하지 않음
+	println(number)
 	
 }
 
+// 	늦은 초기화, Dart의 late이랑 같은 키워드
+//  초기화하기 전까지는 변수 사용이 불가능
+//  기본 자료형에는 사용할수 없음
+//  lateinit 변수의 초기화 여부를 확인 하는 방법은 ::{변수}.isInitialized
 
-class FoodCourt {
-	fun searchPrice(foodName: String) {
-		
-		val price = when (foodName) {
-			FOOD_CREAM_PASTA -> 13000
-			FOOD_STEAK -> 25000
-			FOOD_PIZZA -> 15000
-			else -> 0
+class LateInitSample {
+	lateinit var text: String
+	
+	fun getLateInitText(): String {
+		if (::text.isInitialized) {
+			return text
+		} else {
+			return "기본값"
 		}
-		
-		println("${foodName}의 가격은 ${price}원 입니다.")
-	}
-	
-	
-	// companion object은 인스턴스에 상관없이 모든 인스턴스가 공유하는 객체를 만들고 싶을 때 사용한다.
-	// 익명으로도 가능하고, 이름을 붙이는 것도 가능하다.
-	companion object {
-		const val FOOD_CREAM_PASTA = "크림파스타"
-		const val FOOD_STEAK = "스테이크"
-		const val FOOD_PIZZA = "피자"
 	}
 }
